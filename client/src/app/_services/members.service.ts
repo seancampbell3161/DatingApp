486558b41abc7215675758c57275bdc2636031b3
seancampbell3161 <sean.campbell3161@gmail.com>
2022-05-14 19:00:02 -0500

    End of section 10

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_models/member';
 
@@ -16,17 +18,38 @@ export class MembersService {
 
   baseUrl = environment.apiUrl;
 
+  public members: Member[] = [];
+
   constructor(private http: HttpClient) { }
 
   getMembers() {
-    return this.http.get<Member[]>(this.baseUrl + 'users');
+    // see if we already have the members
+    if (this.members.length > 0) return of(this.members);
+
+    return this.http.get<Member[]>(this.baseUrl + 'users').pipe(
+      map(members => {
+        this.members = members;
+        return members;
+      })
+    )
   }
 
   getMember(username: string) {
+    // see if we already have member
+    const member = this.members.find(m => m.username === username);
+    if (member !== undefined) return of(member);
+
+    // if we don't have the member already
     return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
 
   updateMember(member: Member) {
-    return this.http.put(this.baseUrl + 'users', member);
+    return this.http.put(this.baseUrl + 'users', member).pipe(
+      map(() => {
+        // updates our array of users with the current updated user
+        const index =  this.members.indexOf(member);
+        this.members[index] = member;
+      })
+    )
   }
 }
