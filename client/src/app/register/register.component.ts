5bbed219cef39142ffa6474909edf9493e264c34
seancampbell3161 <sean.campbell3161@gmail.com>
2022-07-31 12:09:40 -0500

    actual error fix

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -56,7 +56,7 @@ export class RegisterComponent implements OnInit {
       },
       error => {
         console.log(error);
-        this.validationErrors = error;
+        this.validationErrors = error.error;
       }
     );
   }
