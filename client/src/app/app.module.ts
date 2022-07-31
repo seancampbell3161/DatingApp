8a7e24c576743f534b5dfa5dda1c76d8c6c38418
seancampbell3161 <sean.campbell3161@gmail.com>
2022-07-30 22:57:34 -0500

    pre postgres switch

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -72,7 +72,7 @@ import { ConfirmDialogComponent } from './modals/confirm-dialog/confirm-dialog.c
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
     { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
-    // { provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true },
   ],
   bootstrap: [AppComponent]
 })
